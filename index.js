d241185f5b44606b7ffeb45b6a4bc2dfdd99583d
Carlos <carcastrojara@gmail.com>
2023-05-04 19:56:48 -0400

    update email

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ dotenv.config();
 conectarDB();
 
 //Configurar CORS
-const whitelist = [process.env.FRONTEND_URL];
+const whitelist = [process.env.FRONTEND_URL, 
+                  process.env.DATABASE_URI];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.includes(origin)) {
